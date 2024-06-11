e1ce3e27b4fa00ebc16816f0ebe1f54d7ac1f35f
Abubakr780 <142640136+Abubakr780@users.noreply.github.com>
2024-06-11 08:51:47 +0000

    01_verables & 02_datatype

---
+++ 01_Basic/01_veriables.js
@@ -0,0 +1,15 @@
+const accountId=12345
+let accountEmail="Abubakrsiddique780@gmail.com"
+var accountPassword=12345
+accountCity="Melaka"
+let accountStatus
+
+// accountid=123 //Not Allowed
+/*
+Prefer don't use var datatype in js
+because of the issue in block scope and functional scope
+*/
+
+//console.log(accountEmail);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountStatus])
---
+++ 01_Basic/02_datatype.js
@@ -0,0 +1,24 @@
+"use strict" // treat all js code as a newer version
+
+// alert(3+3) //We are using a node.js , not using a browser
+
+console.log(3+3)
+console.log("Abubakr")
+
+let name="Abubakr"
+let age=27
+let isLoggin=true
+let state= null
+
+// number => 2 pewr 53
+// bigint
+// string => ""
+// boolean => true/ false 
+// null => standalone value
+// undefined 
+// symbol => check unique then we use symbol
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null)// object
+
