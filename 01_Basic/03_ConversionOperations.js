652275741afa1d6978242e756067968649819b3a
Abubakr780 <142640136+Abubakr780@users.noreply.github.com>
2024-06-11 09:26:19 +0000

    Conversion of Datatypes

---
+++ 01_Basic/03_ConversionOperations.js
@@ -0,0 +1,29 @@
+let score = undefined
+//console.log( typeof score)
+//console.log(typeof (score))
+
+let ValueInNumber = Number(score)
+//console.log(typeof ValueInNumber)
+//console.log(ValueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// null => 0
+// true => 1
+// undefined => NaN
+
+let isLoggedIn = "abc"
+
+let boleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof boleanIsLoggedIn)
+console.log(boleanIsLoggedIn)
+
+// 1 => true, 0 => false
+// "" => false
+// "abc" => true
+
+let someNumber = 33
+
+let stringNumbers=String(someNumber)
+console.log(typeof stringNumbers)
+console.log(stringNumbers)
