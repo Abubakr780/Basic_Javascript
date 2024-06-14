4a397b9d1ecfa4b95922452c56df362b42944dcb
Abubakr780 <142640136+Abubakr780@users.noreply.github.com>
2024-06-14 09:36:28 +0000

    05_string2

--- 01_Basic/05_String.js
+++ 01_Basic/05_String.js
@@ -6,10 +6,32 @@ const repocount=1
 //console.log(typeof name)
 //console.log(`hello  my name is ${name} and my repocount is ${repocount}`)
 
-const gameName= new String('abubakr')
+const gameName= new String(' abubakr_pakistan')
 
 //console.log(gameName[0])
-console.log(gameName.length)
-console.log(gameName.toUpperCase())
-console.log(gameName.charAt(2))
-console.log(gameName.indexOf('r'))
+//console.log(gameName.length)
+//console.log(gameName.toUpperCase())
+//console.log(gameName.charAt(2))
+///console.log(gameName.indexOf('r'))
+
+const newString = gameName.substring(2,7)
+//console.log(newString)
+
+const anotherString= gameName.slice(1,4)
+//console.log(anotherString)
+
+const myString="     Pakistan       "
+//console.log(myString)
+//console.log(myString.trim())
+
+const url="http://abubakr.com/abubakr%20value"
+//console.log(url.replace('%20','_'))
+
+// console.log(gameName.includes('pakistan'))
+
+// console.log(gameName.split('_'))
+
+//console.log(gameName.substring(8,15))
+//console.log(gameName.trim())
+console.log(gameName.replace('_','/'))
+console.log()
